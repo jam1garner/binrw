a44d56cf700e39604b2fd5bc51ea9566610eed68
Colin Snover <github.com@zetafleet.com>
2021-02-23 22:59:40 -0600

    Clean up custom keywords definitions

--- binread_derive/src/parser/keywords.rs
+++ binread_derive/src/parser/keywords.rs
@@ -1,54 +1,45 @@
-macro_rules! kws {
-    ($($idents:ident),+ $(,)?) => {
+macro_rules! define_keywords {
+    ($($keyword:ident),+ $(,)?) => {
         $(
-            syn::custom_keyword!($idents);
+            syn::custom_keyword!($keyword);
         )*
     }
 }
 
-kws! {
-    // attribute description
-    br,
+define_keywords! {
+    align_after,
+    align_before,
+    args,
+    args_tuple,
+    assert,
+    big,
     binread,
-
-    // all level
-    big,        // big
-    little,     // little
-    magic,      // magic = [lit]
-    assert,     // assert(expr)
-    pre_assert,     // pre_assert(expr)
-
-    // top-level
-    import,     // import(expr, ..)
-    import_tuple, // import(expr)
+    br,
+    calc,
+    count,
+    default,
+    deref_now,
+    ignore,
+    import,
+    import_tuple,
+    is_big,
+    is_little,
+    little,
+    magic,
+    map,
+    offset,
+    offset_after,
+    pad_after,
+    pad_before,
+    pad_size_to,
+    parse_with,
+    postprocess_now,
+    pre_assert,
     repr,
+    restore_position,
     return_all_errors,
     return_unexpected_error,
-
-    // field-level
-    map,        // map = [func]
-    try_map,    // try_map = [func]
-    parse_with, // parse_with = [func]
-    calc,       // calc = [expr]
-    count,      // count = [expr]
-    is_little,  // is_little = [expr]
-    is_big,     // is_big = [expr]
-    args,       // args(expr, ..)
-    args_tuple, // args_tuple = [expr]
-    default,    // default
-    ignore,     // ignore
-    deref_now,  // deref_now
-    restore_position,// restore_position
-    postprocess_now, // postprocess_now
-    offset,     // offset(expr)
-    offset_after,     // offset(expr)
-    /*if,*/     // if(expr)
+    seek_before,
     temp,
-
-    pad_before,   // pad_before(expr)
-    pad_after,    // pad_after(expr)
-    align_before, // align_before(expr)
-    align_after,  // align_after(expr)
-    seek_before,  // seek_before(expr)
-    pad_size_to,  // pad_size_to(expr)
+    try_map,
 }
