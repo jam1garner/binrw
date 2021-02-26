28a43553ed689adabcf396dfed6b3cc6eda254fd
Colin Snover <github.com@zetafleet.com>
2021-02-25 22:16:22 -0600

    Clean up struct tests

--- binread/tests/derive_struct.rs
+++ binread/tests/derive_struct.rs
@@ -1,100 +1,120 @@
-use binread::{BinRead, BinResult, io::{Cursor, Read, Seek, SeekFrom}, FilePtr, NullString, ReadOptions};
-
-use binread::BinReaderExt;
-
-#[derive(Debug)]
-struct BadDifferenceError(u16);
-
-#[derive(BinRead, Debug)]
-#[br(big, magic = b"TEST")]
-#[br(assert(entries.len() as u32 == extra_entry_count + 1))]
-struct TestFile {
-    extra_entry_count: u32,
-
-    #[br(count = extra_entry_count + 1, args(0x69))]
-    entries: Vec<FilePtr<u32, TestEntry>>,
-
-    #[br(default)]
-    start_as_none: Option<NotBinWrite>,
-
-    #[br(calc = 1 + 2)]
-    calc_test: u32
-}
-
-#[derive(Debug)]
-struct NotBinWrite {}
-
-fn read_offsets<R: Read + Seek>(reader: &mut R, ro: &ReadOptions, _: ())
-    -> BinResult<(u16, u16)>
-{
-    Ok((
-        u16::read_options(reader, ro, ())?,
-        u16::read_options(reader, ro, ())?
-    ))
-}
-
-#[derive(BinRead, Debug)]
-#[br(little, magic = b"TST2")]
-#[br(import(extra_val: u8))]
-struct TestEntry {
-    #[br(map = |val: u32| val.to_string())]
-    entry_num: String,
-
-    #[br(seek_before(SeekFrom::Current(4)))]
-    #[br(parse_with = read_offsets)]
-    //#[br(is_big = "entry_num == \"1\"")]
-    offsets: (u16, u16),
-
-    #[br(assert(
-          /*assertion = */ offsets.1 - offsets.0 == 0x10,
-        /*raise error = */ BadDifferenceError(offsets.1 - offsets.0)
-    ))]
-    #[br(if(offsets.0 == 0x20))]
-    name: Option<FilePtr<u32, NullString>>,
-
-    #[br(ignore)]
-    extra_val: u8,
-}
-
-const TEST_CONTENTS: &[u8] = include_bytes!("./test_file.bin");
+use binread::{BinRead, BinResult, derive_binread, io::{Cursor, Read, Seek, SeekFrom}, FilePtr, NullString, ReadOptions};
 
 #[test]
-fn test_read() {
-    let mut test = Cursor::new(TEST_CONTENTS);
-    let test_file: TestFile = test.read_le().unwrap();
-    dbg!(test_file);
+fn all_the_things() {
+    #[derive(Debug)]
+    struct BadDifferenceError(u16);
+
+    #[derive(Debug)]
+    struct PlainObject;
+
+    #[derive(BinRead, Debug)]
+    #[br(big, magic = b"TEST")]
+    #[br(assert(entries.len() as u32 == extra_entry_count + 1))]
+    struct Test {
+        extra_entry_count: u32,
+
+        #[br(count = extra_entry_count + 1, args(0x69))]
+        entries: Vec<FilePtr<u32, TestEntry>>,
+
+        #[br(default)]
+        start_as_none: Option<PlainObject>,
+
+        #[br(calc = 1 + 2)]
+        calc_test: u32
+    }
+
+    fn read_offsets<R: Read + Seek>(reader: &mut R, ro: &ReadOptions, _: ())
+        -> BinResult<(u16, u16)>
+    {
+        Ok((
+            u16::read_options(reader, ro, ())?,
+            u16::read_options(reader, ro, ())?
+        ))
+    }
+
+    #[derive(BinRead, Debug)]
+    #[br(little, magic = b"TST2")]
+    #[br(import(extra_val: u8))]
+    struct TestEntry {
+        #[br(map = |val: u32| val.to_string())]
+        entry_num: String,
+
+        #[br(assert(
+            offsets.1 - offsets.0 == 0x10,
+            BadDifferenceError(offsets.1 - offsets.0)
+        ))]
+        #[br(seek_before(SeekFrom::Current(4)))]
+        #[br(parse_with = read_offsets)]
+        #[br(is_big = entry_num == "1")]
+        offsets: (u16, u16),
+
+        #[br(if(offsets.0 == 0x20))]
+        name: Option<FilePtr<u32, NullString>>,
+
+        #[br(ignore)]
+        extra_val: u8,
+    }
+
+    Test::read(&mut Cursor::new(include_bytes!("data/test_file.bin"))).unwrap();
 }
 
-const BAD_TEST_CONTENTS: &[u8] = include_bytes!("./test_file_bad.bin");
-
 #[test]
-fn test_assert_fail() {
-    let mut test = Cursor::new(BAD_TEST_CONTENTS);
-    let err = test.read_le::<TestFile>()
-        .expect_err("Offset assertion should have failed");
-    let custom_err = err.custom_err::<BadDifferenceError>().expect("Error type was lost");
-    assert_eq!(custom_err.0, 0xBAAD - 0x20, "Unexpected failure value");
+fn assert() {
+    #[derive(BinRead, Debug)]
+    struct Test {
+        #[br(assert(a == 1))]
+        a: u8,
+    }
+
+    Test::read(&mut Cursor::new("\x01")).unwrap();
+    let error = Test::read(&mut Cursor::new("\0")).expect_err("accepted bad data");
+    match error {
+        binread::Error::AssertFail { pos, message } => {
+            // TODO: This position is wrong, but fixing it requires a breaking API
+            // change to `binread::error::assert` since the position of the initial read
+            // must be passed as an argument.
+            assert_eq!(pos, 1);
+            assert_eq!(message, "a == 1");
+        },
+        _ => panic!("bad error type"),
+    }
 }
 
-#[derive(BinRead, Debug)]
-#[br(big, magic = b"TEST")]
-struct TestTupleStruct (
-    u32,
+#[test]
+fn assert_custom_err() {
+    #[derive(Debug)]
+    struct Oops(u8);
 
-    #[br(count = self_0 + 1, args(0x69))]
-    Vec<FilePtr<u32, TestEntry>>,
+    #[derive(BinRead, Debug)]
+    struct Test {
+        #[br(assert(a == 1, Oops(a)))]
+        a: u8,
+    }
 
-    #[br(default)]
-    Option<NotBinWrite>,
-
-    #[br(calc = 1 + 2)]
-    u32
-);
+    Test::read(&mut Cursor::new("\x01")).unwrap();
+    let error = Test::read(&mut Cursor::new("\x02")).expect_err("accepted bad data");
+    let error = error.custom_err::<Oops>().expect("bad error type");
+    assert_eq!(error.0, 2);
+}
 
 #[test]
-fn test_tuple() {
-    let mut test = Cursor::new(TEST_CONTENTS);
-    dbg!(TestTupleStruct::read(&mut test).unwrap());
+fn calc_temp_field() {
+    #[derive_binread]
+    #[derive(Debug, PartialEq)]
+    #[br(big)]
+    struct Test {
+        #[br(temp)]
+        len: u32,
+
+        #[br(count = len)]
+        vec: Vec<u8>,
+    }
+
+    let result = Test::read(&mut Cursor::new(b"\0\0\0\x05ABCDE")).unwrap();
+    // This also indirectly checks that `temp` is actually working since
+    // compilation would fail if it weren’t due to missing the `len` field
+    assert_eq!(result, Test { vec: Vec::from(&b"ABCDE"[..]) });
 }
 
 #[test]
@@ -109,8 +129,7 @@ fn deref_now() {
         b: i32,
     }
 
-    let mut reader = Cursor::new(include_bytes!("data/deref_now.bin"));
-    let result = Test::read(&mut reader).unwrap();
+    let result = Test::read(&mut Cursor::new(include_bytes!("data/deref_now.bin"))).unwrap();
     assert_eq!(result, Test {
         a: FilePtr { ptr: 0x10, value: Some(NullString(b"Test string".to_vec())) },
         b: -1,
@@ -130,4 +149,26 @@ fn try_directive() {
     assert!(result.a.is_none());
     let result = Test::read(&mut Cursor::new(b"\xff\xff\xff\xff\0\0\0\0")).unwrap();
     assert_eq!(result.a, Some([ -1, 0 ]));
+}
+
+#[test]
+fn tuple() {
+    #[derive(BinRead, Debug, Eq, PartialEq)]
+    struct Test(#[br(big)] u16, #[br(little)] u16);
+
+    let result = Test::read(&mut Cursor::new("\0\x01\x02\0")).unwrap();
+    assert_eq!(result, Test(1, 2));
+}
+
+#[test]
+fn tuple_calc_temp_field() {
+    #[derive_binread]
+    #[derive(Debug, Eq, PartialEq)]
+    #[br(big)]
+    struct Test(#[br(temp)] u16, #[br(calc((self_0 + 1).into()))] u32);
+
+    let result = Test::read(&mut Cursor::new(b"\0\x04")).unwrap();
+    // This also indirectly checks that `temp` is actually working since
+    // compilation would fail if it weren’t due to missing a second item
+    assert_eq!(result, Test(5u32));
 }
