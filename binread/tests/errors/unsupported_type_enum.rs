dcf48dd271226cd6cb85831758c963b1caee7cda
Colin Snover <github.com@zetafleet.com>
2021-02-27 14:29:44 -0600

    Test unsupported types using derive_binread also

--- binread/tests/errors/unsupported_type_enum.rs
+++ binread/tests/errors/unsupported_type_enum.rs
@@ -1,6 +1,9 @@
-use binread::BinRead;
+use binread::{BinRead, derive_binread};
 
 #[derive(BinRead)]
 enum Foo {}
+
+#[derive_binread]
+enum Bar {}
 
 fn main() {}
--- binread/tests/errors/unsupported_type_union.rs
+++ binread/tests/errors/unsupported_type_union.rs
@@ -1,6 +1,11 @@
-use binread::BinRead;
+use binread::{BinRead, derive_binread};
 
 #[derive(BinRead)]
+union Foo {
+    a: i32,
+}
+
+#[derive_binread]
 union Bar {
     a: i32,
 }
