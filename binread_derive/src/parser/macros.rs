253f4532ea56495ca5326b6461276acd9335f265
Colin Snover <github.com@zetafleet.com>
2021-03-04 23:23:52 -0600

    Document the theory behind `attr_struct` macro

--- binread_derive/src/parser/macros.rs
+++ binread_derive/src/parser/macros.rs
@@ -30,6 +30,32 @@ macro_rules! parse_any {
     };
 }
 
+// The way this works sucks for a couple reasons which are not really worth
+// dealing with right now, but maybe are worth dealing with in the future:
+//
+// 1. Using a separate enum just for parsing, instead of implementing parsing
+// within a generated struct, shouldn’t really be necessary, but seemed to be
+// the simplest to make everything work within the confines of the syn API.
+// There is no way to get a `ParseStream` in syn other than to implement
+// `syn::parse::Parse`, and that API return signature is `Result<Self>`, but the
+// parser should to be able to return partial results instead (as it does now),
+// so it’d be necessary to instead implement `Parse` for `PartialResult` and
+// then go through an internal API that actually does parsing (and probably also
+// reimplements other stuff like `Punctuated` since there would no longer be a
+// type containing all the possible directives). It would be possible also to
+// attach errors to the structs themselves, but it did not seem like the extra
+// work to move non-fatal errors there was really worth the added effort since
+// the current design was already written and functioning.
+//
+// 2. The variant-to-field mapping is awful. The `from` attributes should be
+// taking types instead of idents, but can’t because then there would be no way
+// to generate the enum variants. Variant names could be provided separately,
+// but this would clutter the call sites for no particularly good reason—the
+// types are normalised enough that it’s possible to just fill out the rest of
+// the type here, even though it’s a nasty obfuscation that will confuse anyone
+// that doesn’t look at what the macro is doing.
+//
+// So, you know… here be dragons, and I’m sorry in advance.
 macro_rules! attr_struct {
     (
         #[from($attr_ty:ident)]
