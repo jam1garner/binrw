d4704ac93a7137d18593c6b1378a6a43f590f93c
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-03-10 09:27:04 -0500

    Add test for endian string conversion

--- binread/src/endian.rs
+++ binread/src/endian.rs
@@ -17,10 +17,10 @@ pub use Endian::{
     Native as NE
 };
 
-impl Into<String> for &Endian {
-    fn into(self) -> String {
+impl From<&Endian> for String {
+    fn from(endian: &Endian) -> String {
         String::from(
-            match self {
+            match endian {
                 Endian::Big => "Big",
                 Endian::Little => "Little",
                 Endian::Native => "Native",
@@ -32,5 +32,22 @@ impl Into<String> for &Endian {
 impl Default for Endian {
     fn default() -> Endian {
         Endian::Native
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn endian_to_string() {
+        for &(ref endian, string) in [
+            (Endian::Big, "Big"),
+            (Endian::Little, "Little"),
+            (Endian::Native, "Native"),
+        ].iter() {
+            let converted: String = endian.into();
+            assert_eq!(converted, string)
+        }
     }
 }
