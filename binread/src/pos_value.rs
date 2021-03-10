180c5e6dbafdf0ef316737ad0fb96507c9a000fc
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-03-10 09:05:03 -0500

    Add tests for PosValue

--- binread/src/pos_value.rs
+++ binread/src/pos_value.rs
@@ -75,3 +75,32 @@ impl<U, T: PartialEq<U>> PartialEq<U> for PosValue<T> {
         self.val == *other
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate as binread;
+
+    #[test]
+    fn pos_value() {
+        use binread::{BinRead, PosValue, BinReaderExt, io::Cursor};
+
+        #[derive(BinRead)]
+        struct MyType {
+            a: u16,
+            b: PosValue<u8>
+        }
+
+        let mut val = Cursor::new(b"\xFF\xFE\xFD").read_be::<MyType>().unwrap();
+        assert_eq!(val.a, 0xFFFE);
+        assert_eq!(val.b.pos, 2);
+        assert_eq!(*val.b, 0xFD);
+        assert_eq!(val.b, 0xFDu8);
+
+        *val.b = 1u8;
+        assert_eq!(*val.b, 1);
+        assert_eq!(format!("{:?}", val.b), "1");
+        let clone = val.b.clone();
+        assert_eq!(*clone, *val.b);
+        assert_eq!(clone.pos, val.b.pos);
+    }
+}
