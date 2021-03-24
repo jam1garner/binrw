3e22617ff4e5d7bfecc29e1a1e90903824c0a9f5
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-03-24 16:27:18 -0400

    Add SatisfiedOrOptional trait

--- binrw/src/builder_types.rs
+++ binrw/src/builder_types.rs
@@ -6,3 +6,12 @@ pub struct Optional;
 
 #[doc(hidden)]
 pub struct Needed;
+
+// TODO: seal?
+/// Indicates that a requirement for a typed builder has been met, either by
+/// the user providing one, or by a default being given.
+#[doc(hidden)]
+pub trait SatisfiedOrOptional {}
+
+impl SatisfiedOrOptional for Satisfied {}
+impl SatisfiedOrOptional for Optional {}
