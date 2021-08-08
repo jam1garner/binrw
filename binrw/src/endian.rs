ad4d85469938bdfa8094f713401ceb951e961de6
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-08-08 00:06:44 -0400

    Add byte order marker conversion for `Endian`

--- binrw/src/endian.rs
+++ binrw/src/endian.rs
@@ -1,5 +1,7 @@
 //! Type definitions for byte order handling.
 
+use crate::BinResult;
+
 /// Defines the order of bytes in a multi-byte type.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Endian {
@@ -26,5 +28,32 @@ impl core::fmt::Display for Endian {
 impl Default for Endian {
     fn default() -> Endian {
         Endian::Native
+    }
+}
+
+const BOM: u16 = 0xFEFF;
+const REVERSE_BOM: u16 = 0xFFFE;
+
+impl Endian {
+    /// Converts from a UTF-16 BOM (either `[0xFF, 0xFE]` or `[0xFE, 0xFF]`) into the endian it
+    /// represents
+    pub fn from_utf16_bom_bytes(bom: [u8; 2]) -> BinResult<Self> {
+        match u16::from_le_bytes(bom) {
+            BOM => Ok(Self::Little),
+            REVERSE_BOM => Ok(Self::Big),
+            _ => Err(crate::Error::BadMagic {
+                pos: u64::MAX,
+                found: Box::new("Invalid UTF-16 BOM")
+            })
+        }
+    }
+
+    /// Converts endian to a UTF-16 BOM representing the given endian
+    pub fn into_utf16_bom_bytes(&self) -> [u8; 2] {
+        match self {
+            Self::Little => u16::to_le_bytes(BOM),
+            Self::Big => u16::to_be_bytes(BOM),
+            Self::Native => u16::to_ne_bytes(BOM),
+        }
     }
 }
