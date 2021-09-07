61eae08ef31a4c06a042efd0eaf0c3b2f9a2ab79
David Golembiowski <david@dgolembiowski.com>
2021-09-06 23:38:54 -0400

    It appears that the macro is not exporting properly. Any ideas?

--- binrw/tests/derive/write/simple.rs
+++ binrw/tests/derive/write/simple.rs
@@ -25,8 +25,25 @@ use binrw::BinReaderExt;
 
 #[binread]
 #[binwrite]
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct TestRoundTrip {
+    x: u16,
+
+    #[brw(little)]
+    y: u16,
+
+    #[brw(is_big = true)]
+    z: u32,
+
+    #[brw(is_big = false)]
+    not_z: u32,
+}
+
+use binrw::binrw_attr;
+
+#[binrw]
+#[derive(Debug, PartialEq)]
+struct TestRoundTripConjugate {
     x: u16,
 
     #[brw(little)]
@@ -53,4 +70,25 @@ fn round_trip() {
         .unwrap();
 
     assert_eq!(&x.into_inner()[..], bytes);
+}
+
+#[test]
+fn round_trip_2() {
+    let bytes = &[0, 1, 2, 0, 0, 0, 0, 3, 3, 0, 0, 0];
+    let mut reader = Cursor::new(bytes);
+    let test: TestRoundTrip = reader.read_be().unwrap();
+    
+    let bytes_conj = &[0, 1, 2, 0, 0, 0, 0, 3, 3, 0, 0, 0];
+    let mut reader_conj = Cursor::new(bytes_conj);
+    let conj: TestRoundTripConjugate = reader_conj.read_be().unwrap();
+    
+    let mut x = Cursor::new(Vec::new());
+    let mut y = Cursor::new(Vec::new());
+
+    test.write_options(&mut x, &WriteOptions::new(Endian::Big), ())
+        .unwrap();
+    conj.write_options(&mut y, &WriteOptions::new(Endian::Big), ())
+        .unwrap();
+
+    assert_eq!(&x.into_inner()[..] == bytes, bytes_conj == &y.into_inner()[..]);
 }
