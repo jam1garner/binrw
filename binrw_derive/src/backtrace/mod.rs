552634d91f15e9a4957b3ce6bdac163cc03eb6c7
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-09-19 02:50:59 -0400

    Add initial syntax highlighting for backtrace code

--- binrw_derive/src/backtrace/mod.rs
+++ binrw_derive/src/backtrace/mod.rs
@@ -1,45 +1,41 @@
 #![allow(clippy::non_ascii_literal)]
-#![allow(unused_imports, unused_variables, dead_code)]
-use std::{
-    collections::HashMap,
-    fmt::{self, Display, Formatter},
-    ops::Range,
-};
+//#![allow(unused_imports, unused_variables, dead_code)]
+use std::fmt::{self, Display, Formatter};
+
+mod syntax_highlighting;
+use owo_colors::OwoColorize;
+use syntax_highlighting::SyntaxInfo;
 
 use crate::parser::read::StructField;
 use proc_macro2::Span;
-use syn::{spanned::Spanned, Type};
+use syn::spanned::Spanned;
 
-#[derive(Debug)]
 pub(crate) struct BacktraceFrame {
     span: Span,
-    ty: Type,
+    //ty: Type,
     highlight_line: usize,
+    syntax_info: SyntaxInfo,
 }
 
-//struct SyntaxInfo {
-//    lines: HashMap<usize, LineSyntax>,
-//}
-//
-//enum Color {
-//
-//}
-//
-//struct LineSyntax {
-//    highlights: Vec<(Range<usize>, Color)>,
-//}
+#[cfg(nightly)]
+struct Line {
+    line_num: usize,
+    start_col: usize,
+    line: String,
+}
 
 impl BacktraceFrame {
     pub(crate) fn from_field(field: &StructField) -> Self {
         Self {
             span: field.span,
             highlight_line: field.ty.span().start().line,
-            ty: field.ty.clone(),
+            syntax_info: syntax_highlighting::get_syntax_highlights(field),
+            //ty: field.ty.clone(),
         }
     }
 
     #[cfg(nightly)]
-    fn iter_lines(&self) -> impl Iterator<Item = (usize, String)> + '_ {
+    fn iter_lines(&self) -> impl Iterator<Item = Line> + '_ {
         if let Some(text) = self.span.unwrap().source_text() {
             let start_col = self.span.start().column - 1;
             let mut min_whitespace = start_col;
@@ -54,7 +50,7 @@ impl BacktraceFrame {
 
             (self.span.start().line..)
                 .zip(text.lines().enumerate().map(|(i, line)| {
-                    if i == 0 {
+                    let line = if i == 0 {
                         let spaces_to_add = start_col - min_whitespace;
                         if spaces_to_add == 0 {
                             line.to_owned()
@@ -63,8 +59,15 @@ impl BacktraceFrame {
                         }
                     } else {
                         line[min_whitespace..].to_owned()
-                    }
+                    };
+
+                    (min_whitespace + 1, line)
                 }))
+                .map(|(line_num, (start_col, line))| Line {
+                    line_num,
+                    start_col,
+                    line,
+                })
                 .collect::<Vec<_>>()
                 .into_iter()
         } else {
@@ -72,11 +75,67 @@ impl BacktraceFrame {
         }
     }
 
-    fn write_line(&self, line_num: usize, line: &str, f: &mut Formatter<'_>) -> fmt::Result {
-        if line_num == self.highlight_line {
-            writeln!(f, "   {} ⎬  {}", line_num, line)
+    fn write_line(
+        &self,
+        Line {
+            line_num,
+            start_col,
+            line,
+        }: Line,
+        f: &mut Formatter<'_>,
+    ) -> fmt::Result {
+        let should_highlight = line_num == self.highlight_line;
+
+        if should_highlight {
+            write!(f, "   {} {}  ", line_num.bold(), "⎬".bold())?;
         } else {
-            writeln!(f, "   {} │  {}", line_num, line)
+            write!(f, "   {} │  ", line_num)?;
+        }
+
+        if let Some(line_highlights) = self.syntax_info.lines.get(&line_num) {
+            // syntax highlighting on this line
+            let highlights = line_highlights.highlights.iter();
+            let highlights_next_start = line_highlights
+                .highlights
+                .iter()
+                .skip(1)
+                .map(|x| x.0.start)
+                .chain(std::iter::once(start_col + line.len()));
+
+            if let Some((first_range, _)) = line_highlights.highlights.get(0) {
+                let component = &line[..first_range.start - start_col];
+
+                if should_highlight {
+                    write!(f, "{}", component.bold())?;
+                } else {
+                    write!(f, "{}", component)?;
+                }
+            }
+
+            for ((range, color), next_start) in highlights.zip(highlights_next_start) {
+                let range = (range.start - start_col)..(range.end - start_col);
+                let next_start = next_start - start_col;
+                let uncolored_range = range.end..next_start;
+
+                // write colored portion
+                if should_highlight {
+                    write!(f, "{}", (&line[range]).bold().color(color.into_owo()))?;
+                } else {
+                    write!(f, "{}", (&line[range]).color(color.into_owo()))?;
+                }
+
+                // write next uncolored portion
+                if should_highlight {
+                    write!(f, "{}", (&line[uncolored_range]).bold())?;
+                } else {
+                    write!(f, "{}", &line[uncolored_range])?;
+                }
+            }
+
+            writeln!(f)
+        } else {
+            // no syntax highlighting on this line
+            writeln!(f, "{}", line)
         }
     }
 }
@@ -91,8 +150,8 @@ impl Display for BacktraceFrame {
 
         //writeln!(f)?;
         writeln!(f, "  ┄{}─╮", bars)?;
-        for (line_num, line) in self.iter_lines() {
-            self.write_line(line_num, &line, f)?;
+        for line in self.iter_lines() {
+            self.write_line(line, f)?;
         }
         writeln!(f, "  ┄{}─╯", bars)?;
 
---
+++ binrw_derive/src/backtrace/syntax_highlighting.rs
@@ -0,0 +1,234 @@
+use std::{collections::HashMap, ops::Range};
+
+use crate::parser::read::StructField;
+use owo_colors::XtermColors;
+use syn::visit::{self, /*visit_expr,*/ visit_type, Visit};
+
+#[derive(Default)]
+pub(crate) struct SyntaxInfo {
+    pub(crate) lines: HashMap<usize, LineSyntax>,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub(crate) enum Color {
+    String,   // yellow
+    Char,     // purple
+    Number,   // purple
+    Keyword,  // red
+    Function, // green
+}
+
+impl Color {
+    pub(crate) fn into_owo(self) -> owo_colors::XtermColors {
+        match self {
+            Self::String => XtermColors::DollyYellow,
+            Self::Char | Self::Number => XtermColors::Heliotrope,
+            Self::Keyword => XtermColors::DarkRose,
+            Self::Function => XtermColors::RioGrandeGreen,
+        }
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct LineSyntax {
+    pub(crate) highlights: Vec<(Range<usize>, Color)>,
+}
+
+#[derive(Default)]
+struct Visitor {
+    syntax_info: SyntaxInfo,
+}
+
+pub(super) fn get_syntax_highlights(field: &StructField) -> SyntaxInfo {
+    let mut visit = Visitor::default();
+
+    visit_type(&mut visit, &field.ty);
+
+    let Visitor { syntax_info } = visit;
+
+    syntax_info
+}
+
+impl<'ast> Visit<'ast> for Visitor {
+    fn visit_lit(&mut self, lit: &'ast syn::Lit) {
+        let start = lit.span().start();
+        let end = lit.span().end();
+
+        // syntax highlighting for multi-line spans isn't supported yet (sorry)
+        if start.line == end.line {
+            #[allow(clippy::enum_glob_use)]
+            use syn::Lit::*;
+
+            let lines = self
+                .syntax_info
+                .lines
+                .entry(start.line)
+                .or_insert_with(LineSyntax::default);
+
+            lines.highlights.push((
+                start.column..end.column,
+                match lit {
+                    Str(_) | ByteStr(_) => Color::String,
+                    Byte(_) | Char(_) => Color::Char,
+                    Int(_) | Float(_) | Bool(_) => Color::Number,
+                    Verbatim(_) => return,
+                },
+            ));
+        }
+    }
+
+    fn visit_ident(&mut self, ident: &'ast proc_macro2::Ident) {
+        macro_rules! is_any {
+            ($($option:ident),*) => {
+                $(ident == (stringify!($option)) ||)* false
+            }
+        }
+
+        if is_any!(
+            // prelude/keywords/primitives
+            Vec,
+            u8,
+            u16,
+            u32,
+            u64,
+            u128,
+            i8,
+            i16,
+            i32,
+            i64,
+            i128,
+            char,
+            String,
+            Default,
+            Self,
+            super,
+            Drop,
+            Send,
+            Sync,
+            Sized,
+            Fn,
+            FnMut,
+            FnOnce,
+            From,
+            Into,
+            Iterator,
+            IntoIterator,
+            Ord,
+            Eq,
+            PartialEq,
+            Eq,
+            Box,
+            ToString,
+            usize,
+            isize,
+            f32,
+            f64,
+            str,
+            // binrw 'keywords'
+            align_after,
+            align_before,
+            args,
+            args_raw,
+            assert,
+            big,
+            binread,
+            br,
+            brw,
+            binwrite,
+            bw,
+            calc,
+            count,
+            default,
+            deref_now,
+            ignore,
+            import,
+            import_raw,
+            is_big,
+            is_little,
+            little,
+            magic,
+            map,
+            offset,
+            offset_after,
+            pad_after,
+            pad_before,
+            pad_size_to,
+            parse_with,
+            postprocess_now,
+            pre_assert,
+            repr,
+            restore_position,
+            return_all_errors,
+            return_unexpected_error,
+            seek_before,
+            temp,
+            try_map,
+            write_with
+        ) {
+            let start = ident.span().start();
+            let end = ident.span().end();
+
+            self.syntax_info
+                .lines
+                .entry(start.line)
+                .or_insert_with(LineSyntax::default)
+                .highlights
+                .push((start.column..end.column, Color::Keyword));
+        }
+    }
+
+    fn visit_expr_method_call(&mut self, call: &'ast syn::ExprMethodCall) {
+        let ident = &call.method;
+        let start = ident.span().start();
+        let end = ident.span().end();
+
+        self.syntax_info
+            .lines
+            .entry(start.line)
+            .or_insert_with(LineSyntax::default)
+            .highlights
+            .push((start.column..end.column, Color::Function));
+
+        // continue walking ast
+        for attr in &call.attrs {
+            visit::visit_attribute(self, attr);
+        }
+
+        visit::visit_expr(self, &*call.receiver);
+
+        if let Some(turbofish) = call.turbofish.as_ref() {
+            visit::visit_method_turbofish(self, turbofish);
+        }
+
+        for arg in call.args.iter() {
+            visit::visit_expr(self, arg);
+        }
+    }
+
+    fn visit_expr_call(&mut self, call: &'ast syn::ExprCall) {
+        if let syn::Expr::Path(path) = &*call.func {
+            if let Some(ident) = path.path.get_ident() {
+                let start = ident.span().start();
+                let end = ident.span().end();
+
+                self.syntax_info
+                    .lines
+                    .entry(start.line)
+                    .or_insert_with(LineSyntax::default)
+                    .highlights
+                    .push((start.column..end.column, Color::Function));
+            }
+        }
+
+        // continue walking ast
+        for attr in &call.attrs {
+            visit::visit_attribute(self, attr);
+        }
+
+        visit::visit_expr(self, &*call.func);
+
+        for arg in call.args.iter() {
+            visit::visit_expr(self, arg);
+        }
+    }
+}
