42f37440ecbfa49fc47b3f8e5780df2c4911bcd3
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-09-24 15:37:48 -0400

    Add test for backtrace API and position tracking

--- binrw/src/error/backtrace.rs
+++ binrw/src/error/backtrace.rs
@@ -70,23 +70,20 @@ impl ContextExt for Backtrace {
     }
 }
 
-impl<T> ContextExt for Result<T, Error> {
-    /// Adds an additional frame of context to the backtrace
+impl ContextExt for Error {
     fn with_context<Frame: Into<BacktraceFrame>>(self, frame: Frame) -> Self {
-        self.map_err(|err| match err {
+        match self {
             Error::Backtrace(mut backtrace) => {
                 backtrace.frames.push(frame.into());
                 Error::Backtrace(backtrace)
             }
             error => Error::Backtrace(Backtrace::new(error, vec![frame.into()])),
-        })
+        }
     }
 
-    /// Adds an additional frame of context to the backtrace including a message, file name, and
-    /// line number.
     #[track_caller]
     fn with_message(self, message: impl Into<Cow<'static, str>>) -> Self {
-        self.map_err(|err| match err {
+        match self {
             Error::Backtrace(backtrace) => Error::Backtrace(backtrace.with_message(message)),
             error => {
                 let message = message.into();
@@ -109,7 +106,51 @@ impl<T> ContextExt for Result<T, Error> {
                     }],
                 ))
             }
-        })
+        }
+    }
+}
+
+impl<T> ContextExt for Result<T, Error> {
+    /// Adds an additional frame of context to the backtrace
+    fn with_context<Frame: Into<BacktraceFrame>>(self, frame: Frame) -> Self {
+        self.map_err(|err| err.with_context(frame))
+    }
+
+    /// Adds an additional frame of context to the backtrace including a message, file name, and
+    /// line number.
+    #[track_caller]
+    fn with_message(self, message: impl Into<Cow<'static, str>>) -> Self {
+        match self {
+            Err(err) => {
+                let caller = std::panic::Location::caller();
+                Err(match err {
+                    Error::Backtrace(backtrace) => {
+                        Error::Backtrace(backtrace.with_message(message))
+                    }
+                    error => {
+                        let message = message.into();
+                        Error::Backtrace(Backtrace::new(
+                            error,
+                            vec![match message {
+                                Cow::Owned(message) => BacktraceFrame::OwnedFull {
+                                    code: None,
+                                    message,
+                                    file: caller.file(),
+                                    line: caller.line(),
+                                },
+                                Cow::Borrowed(message) => BacktraceFrame::Full {
+                                    code: None,
+                                    message,
+                                    file: caller.file(),
+                                    line: caller.line(),
+                                },
+                            }],
+                        ))
+                    }
+                })
+            }
+            ok => ok,
+        }
     }
 }
 
@@ -161,5 +202,58 @@ pub enum BacktraceFrame {
 impl<T: CustomError + 'static> From<T> for BacktraceFrame {
     fn from(err: T) -> Self {
         Self::Custom(Box::new(err) as _)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn backtrace() {
+        const ERR0: &str = "assert_failed";
+        const ERR1: &str = "while doing something";
+        const ERR2: &str = "then this happened";
+        const ERR3: &str = "and lastly this happened";
+
+        let error = Error::AssertFail {
+            pos: 4,
+            message: ERR0.to_string(),
+        };
+
+        let (line1, error) = (line!(), Err::<(), _>(error.with_message(ERR1)));
+        let (line2, error) = (line!(), error.with_message(ERR2));
+        let error = error.with_context(ERR3);
+
+        dbg!(line1, line2, &error);
+
+        if let Error::Backtrace(backtrace) = error.unwrap_err() {
+            if let Error::AssertFail { pos: 4, message } = &*backtrace.error {
+                assert_eq!(message, ERR0);
+            } else {
+                panic!("Not AssertFail")
+            }
+
+            if let [BacktraceFrame::Full {
+                code: None,
+                message: ERR1,
+                file: file!(),
+                line: l1,
+            }, BacktraceFrame::Full {
+                code: None,
+                message: ERR2,
+                file: file!(),
+                line: l2,
+            }, BacktraceFrame::Custom(last)] = &backtrace.frames[..]
+            {
+                assert_eq!(line1, *l1);
+                assert_eq!(line2, *l2);
+                assert_eq!(last.to_string(), ERR3);
+            } else {
+                panic!("Backtrace incorrect: {:?}", &backtrace.frames)
+            }
+        } else {
+            panic!("Not a backtrace")
+        }
     }
 }
