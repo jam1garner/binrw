5fddc2dad481e35bc1c40f7b4ea18888b908cccc
Colin Snover <github.com@zetafleet.com>
2022-08-26 12:18:11 -0500

    Add test for visibility of calculated write fields

--- binrw/tests/derive/write/calc.rs
+++ binrw/tests/derive/write/calc.rs
@@ -21,3 +21,26 @@ fn calc_simple_write() {
 
     assert_eq!(&x.into_inner()[..], &[1, 0, 2, 0, 0, 0, 3]);
 }
+
+#[test]
+fn calc_visibility() {
+    #[binwrite]
+    struct Test {
+        x: u8,
+
+        #[bw(calc = Some(2))]
+        y: Option<u16>,
+
+        // `y` should be visible here even though it is calculated
+        #[bw(calc = y.unwrap() + 1)]
+        z: u16,
+    }
+
+    let mut x = Cursor::new(Vec::new());
+
+    Test { x: 1 }
+        .write_options(&mut x, &WriteOptions::new(Endian::Big), ())
+        .unwrap();
+
+    assert_eq!(&x.into_inner()[..], &[1, 0, 2, 0, 3]);
+}
