ca761daaed8394f6a345a04265a26f874e2218cc
Colin Snover <github.com@zetafleet.com>
2022-08-28 14:14:21 -0500

    Make write codegen non-generator functions into free functions

--- binrw_derive/src/codegen/write_options/struct_field.rs
+++ binrw_derive/src/codegen/write_options/struct_field.rs
@@ -42,33 +42,6 @@ impl<'a> StructFieldGenerator<'a> {
         }
     }
 
-    fn args_ident(&self) -> Ident {
-        make_ident(&self.field.ident, "args")
-    }
-
-    fn map_fn_ident(&self) -> Ident {
-        make_ident(&self.field.ident, "map_fn")
-    }
-
-    fn specify_endian(&self) -> Option<TokenStream> {
-        match &self.field.endian {
-            CondEndian::Inherited => None,
-            CondEndian::Fixed(endian) => Some({
-                let endian = endian.as_binrw_endian();
-                quote! {
-                    .clone().with_endian(#endian)
-                }
-            }),
-            CondEndian::Cond(endian, cond) => Some({
-                let else_endian = endian.flipped().as_binrw_endian();
-                let endian = endian.as_binrw_endian();
-                quote! {
-                    .clone().with_endian(if #cond { #endian } else { #else_endian })
-                }
-            }),
-        }
-    }
-
     fn prefix_assertions(mut self) -> Self {
         let assertions = get_assertions(&self.field.assertions);
 
@@ -93,7 +66,7 @@ impl<'a> StructFieldGenerator<'a> {
         };
 
         let write_fn = if self.field.map.is_some() {
-            let map_fn = self.map_fn_ident();
+            let map_fn = map_fn_ident(&self.field.ident);
             if self.field.map.is_try() {
                 quote! { #WRITE_FN_TRY_MAP_OUTPUT_TYPE_HINT(&#map_fn, #write_fn) }
             } else {
@@ -112,22 +85,14 @@ impl<'a> StructFieldGenerator<'a> {
         self
     }
 
-    fn field_mapping(&self) -> Option<TokenStream> {
-        match &self.field.map {
-            Map::Try(map_fn) | Map::Map(map_fn) => Some(quote! { (#map_fn) }),
-            Map::Repr(ty) => Some(quote! { (<#ty as core::convert::TryFrom<_>>::try_from) }),
-            Map::None => None,
-        }
-    }
-
     fn prefix_map_fn(mut self) -> Self {
-        let map_fn = self.field_mapping().map(|map_fn| {
-            let map_fn_ident = self.map_fn_ident();
+        let map_fn = field_mapping(&self.field.map).map(|map_fn| {
+            let fn_ident = map_fn_ident(&self.field.ident);
 
             let ty = &self.field.ty;
             let ty_ref = self.field.generated_value().not().then(|| quote! { & });
             quote! {
-                let #map_fn_ident = #WRITE_MAP_INPUT_TYPE_HINT::<#ty_ref #ty, _, _>(#map_fn);
+                let #fn_ident = #WRITE_MAP_INPUT_TYPE_HINT::<#ty_ref #ty, _, _>(#map_fn);
             }
         });
 
@@ -142,8 +107,8 @@ impl<'a> StructFieldGenerator<'a> {
 
     fn write_field(mut self) -> Self {
         let name = &self.field.ident;
-        let args = self.args_ident();
-        let specify_endian = self.specify_endian();
+        let args = args_ident(name);
+        let options = specify_endian(&self.field.endian);
 
         let initialize = match &self.field.read_mode {
             FieldMode::Calc(expr) => Some({
@@ -157,7 +122,7 @@ impl<'a> StructFieldGenerator<'a> {
             _ => None,
         };
 
-        let map_fn = self.field.map.is_some().then(|| self.map_fn_ident());
+        let map_fn = self.field.map.is_some().then(|| map_fn_ident(name));
         let map_try = self.field.map.is_try().then(|| {
             let map_err = super::get_map_err(SAVED_POSITION);
             quote! { #map_err? }
@@ -191,97 +156,12 @@ impl<'a> StructFieldGenerator<'a> {
             #WRITE_FUNCTION (
                 { #store_position &(#map_fn (#name) #map_try) },
                 #WRITER,
-                &#OPT #specify_endian,
+                &#OPT #options,
                 #args
             )?;
         };
 
         self
-    }
-
-    fn pad_before(&self) -> TokenStream {
-        let seek_before = self.field.seek_before.as_ref().map(|seek| {
-            quote! {
-                #SEEK_TRAIT::seek(
-                    #WRITER,
-                    #seek,
-                )?;
-            }
-        });
-        let pad_before = self.field.pad_before.as_ref().map(|padding| {
-            quote! {
-                #WRITE_ZEROES(#WRITER, (#padding) as u64)?;
-            }
-        });
-        let align_before = self.field.align_before.as_ref().map(|alignment| {
-            quote! {{
-                let pos = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
-                let align = ((#alignment) as u64);
-                let rem = pos % align;
-                if rem != 0 {
-                    #WRITE_ZEROES(#WRITER, align - rem)?;
-                }
-            }}
-        });
-        let pad_size_to_before = self.field.pad_size_to.as_ref().map(|_| {
-            quote! {
-                let #BEFORE_POS = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
-            }
-        });
-        let store_position = self.field.restore_position.map(|_| {
-            quote! {
-                let #SAVED_POSITION = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
-            }
-        });
-
-        quote! {
-            #store_position
-            #seek_before
-            #pad_before
-            #align_before
-            #pad_size_to_before
-        }
-    }
-
-    fn pad_after(&self) -> TokenStream {
-        let pad_size_to = self.field.pad_size_to.as_ref().map(|size| {
-            quote! {{
-                let pad_to_size = (#size) as u64;
-                let after_pos = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
-                if let Some(size) = after_pos.checked_sub(#BEFORE_POS) {
-                    if let Some(padding) = pad_to_size.checked_sub(size) {
-                        #WRITE_ZEROES(#WRITER, padding)?;
-                    }
-                }
-            }}
-        });
-        let pad_after = self.field.pad_after.as_ref().map(|padding| {
-            quote! {
-                #WRITE_ZEROES(#WRITER, (#padding) as u64)?;
-            }
-        });
-        let align_after = self.field.align_after.as_ref().map(|alignment| {
-            quote! {{
-                let pos = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
-                let align = ((#alignment) as u64);
-                let rem = pos % align;
-                if rem != 0 {
-                    #WRITE_ZEROES(#WRITER, align - rem)?;
-                }
-            }}
-        });
-        let restore_position = self.field.restore_position.map(|_| {
-            quote! {
-                #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Start(#SAVED_POSITION))?;
-            }
-        });
-
-        quote! {
-            #pad_size_to
-            #pad_after
-            #align_after
-            #restore_position
-        }
     }
 
     fn wrap_condition(mut self) -> Self {
@@ -303,8 +183,8 @@ impl<'a> StructFieldGenerator<'a> {
     fn wrap_padding(mut self) -> Self {
         let out = &self.out;
 
-        let pad_before = self.pad_before();
-        let pad_after = self.pad_after();
+        let pad_before = pad_before(self.field);
+        let pad_after = pad_after(self.field);
         self.out = quote! {
             #pad_before
             #out
@@ -319,7 +199,7 @@ impl<'a> StructFieldGenerator<'a> {
             return self;
         }
 
-        let args = self.args_ident();
+        let args = args_ident(&self.field.ident);
 
         let args_val = if let Some(args) = get_passed_args(self.field) {
             args
@@ -327,7 +207,7 @@ impl<'a> StructFieldGenerator<'a> {
             quote! { () }
         };
 
-        let map_fn = self.map_fn_ident();
+        let map_fn = map_fn_ident(&self.field.ident);
         let out = &self.out;
         self.out = match self.field.read_mode {
             FieldMode::Normal => match &self.field.map {
@@ -369,7 +249,7 @@ impl<'a> StructFieldGenerator<'a> {
     fn prefix_magic(mut self) -> Self {
         if let Some(magic) = &self.field.magic {
             let magic = magic.match_value();
-            let specify_endian = self.specify_endian();
+            let specify_endian = specify_endian(&self.field.endian);
             let out = self.out;
             self.out = quote! {
                 #WRITE_METHOD (
@@ -388,5 +268,125 @@ impl<'a> StructFieldGenerator<'a> {
 
     fn finish(self) -> TokenStream {
         self.out
+    }
+}
+
+fn args_ident(ident: &Ident) -> Ident {
+    make_ident(ident, "args")
+}
+
+fn field_mapping(map: &Map) -> Option<TokenStream> {
+    match map {
+        Map::Try(map_fn) | Map::Map(map_fn) => Some(quote! { (#map_fn) }),
+        Map::Repr(ty) => Some(quote! { (<#ty as core::convert::TryFrom<_>>::try_from) }),
+        Map::None => None,
+    }
+}
+
+fn map_fn_ident(ident: &Ident) -> Ident {
+    make_ident(ident, "map_fn")
+}
+
+fn pad_after(field: &StructField) -> TokenStream {
+    let pad_size_to = field.pad_size_to.as_ref().map(|size| {
+        quote! {{
+            let pad_to_size = (#size) as u64;
+            let after_pos = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
+            if let Some(size) = after_pos.checked_sub(#BEFORE_POS) {
+                if let Some(padding) = pad_to_size.checked_sub(size) {
+                    #WRITE_ZEROES(#WRITER, padding)?;
+                }
+            }
+        }}
+    });
+    let pad_after = field.pad_after.as_ref().map(|padding| {
+        quote! {
+            #WRITE_ZEROES(#WRITER, (#padding) as u64)?;
+        }
+    });
+    let align_after = field.align_after.as_ref().map(|alignment| {
+        quote! {{
+            let pos = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
+            let align = ((#alignment) as u64);
+            let rem = pos % align;
+            if rem != 0 {
+                #WRITE_ZEROES(#WRITER, align - rem)?;
+            }
+        }}
+    });
+    let restore_position = field.restore_position.map(|_| {
+        quote! {
+            #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Start(#SAVED_POSITION))?;
+        }
+    });
+
+    quote! {
+        #pad_size_to
+        #pad_after
+        #align_after
+        #restore_position
+    }
+}
+
+fn pad_before(field: &StructField) -> TokenStream {
+    let seek_before = field.seek_before.as_ref().map(|seek| {
+        quote! {
+            #SEEK_TRAIT::seek(
+                #WRITER,
+                #seek,
+            )?;
+        }
+    });
+    let pad_before = field.pad_before.as_ref().map(|padding| {
+        quote! {
+            #WRITE_ZEROES(#WRITER, (#padding) as u64)?;
+        }
+    });
+    let align_before = field.align_before.as_ref().map(|alignment| {
+        quote! {{
+            let pos = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
+            let align = ((#alignment) as u64);
+            let rem = pos % align;
+            if rem != 0 {
+                #WRITE_ZEROES(#WRITER, align - rem)?;
+            }
+        }}
+    });
+    let pad_size_to_before = field.pad_size_to.as_ref().map(|_| {
+        quote! {
+            let #BEFORE_POS = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
+        }
+    });
+    let store_position = field.restore_position.map(|_| {
+        quote! {
+            let #SAVED_POSITION = #SEEK_TRAIT::seek(#WRITER, #SEEK_FROM::Current(0))?;
+        }
+    });
+
+    quote! {
+        #store_position
+        #seek_before
+        #pad_before
+        #align_before
+        #pad_size_to_before
+    }
+}
+
+fn specify_endian(endian: &CondEndian) -> Option<TokenStream> {
+    match endian {
+        CondEndian::Inherited => None,
+        CondEndian::Fixed(endian) => Some({
+            let endian = endian.as_binrw_endian();
+            quote! {
+                .clone().with_endian(#endian)
+            }
+        }),
+        CondEndian::Cond(endian, cond) => Some({
+            let else_endian = endian.flipped().as_binrw_endian();
+            let endian = endian.as_binrw_endian();
+            quote! {
+                .clone().with_endian(if #cond { #endian } else { #else_endian })
+            }
+        }),
     }
 }
