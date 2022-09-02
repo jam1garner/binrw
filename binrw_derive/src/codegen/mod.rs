2009d85e15d515a50b41b569a5d2fd1f3b46b822
Colin Snover <github.com@zetafleet.com>
2022-09-01 23:44:10 -0500

    Split main codegen function into smaller units

--- binrw_derive/src/codegen/mod.rs
+++ binrw_derive/src/codegen/mod.rs
@@ -15,13 +15,12 @@ use sanitization::{
     ARGS, BINREAD_TRAIT, BINWRITE_TRAIT, BIN_RESULT, OPT, READER, READ_OPTIONS, READ_TRAIT,
     SEEK_TRAIT, WRITER, WRITE_OPTIONS, WRITE_TRAIT,
 };
-use syn::spanned::Spanned;
+use syn::{spanned::Spanned, DeriveInput};
 
 pub(crate) fn generate_impl<const WRITE: bool>(
-    derive_input: &syn::DeriveInput,
+    derive_input: &DeriveInput,
     binrw_input: &ParseResult<Input>,
 ) -> TokenStream {
-    // Generate the argument type name and (if needed) definition
     let (arg_type, arg_type_declaration) = match binrw_input {
         ParseResult::Ok(binrw_input) | ParseResult::Partial(binrw_input, _) => binrw_input
             .imports()
@@ -29,19 +28,7 @@ pub(crate) fn generate_impl<const WRITE: bool>(
         ParseResult::Err(_) => (quote! { () }, None),
     };
 
-    // If there is a parsing error, an impl for the trait still needs to be
-    // generated to avoid misleading errors at all call sites that use the
-    // trait
-    let fn_impl = match binrw_input {
-        ParseResult::Ok(binrw_input) => {
-            if WRITE {
-                write_options::generate(binrw_input, derive_input)
-            } else {
-                read_options::generate(binrw_input, derive_input)
-            }
-        }
-        ParseResult::Partial(_, error) | ParseResult::Err(error) => error.to_compile_error(),
-    };
+    let trait_impl = generate_trait_impl::<WRITE>(binrw_input, derive_input, &arg_type);
 
     let meta_impls = match binrw_input {
         ParseResult::Ok(binrw_input) | ParseResult::Partial(binrw_input, _) => {
@@ -50,9 +37,18 @@ pub(crate) fn generate_impl<const WRITE: bool>(
         ParseResult::Err(_) => None,
     };
 
-    let name = &derive_input.ident;
-    let (impl_generics, ty_generics, where_clause) = derive_input.generics.split_for_impl();
+    quote! {
+        #trait_impl
+        #meta_impls
+        #arg_type_declaration
+    }
+}
 
+fn generate_trait_impl<const WRITE: bool>(
+    binrw_input: &ParseResult<Input>,
+    derive_input: &DeriveInput,
+    arg_type: &TokenStream,
+) -> TokenStream {
     let (trait_name, fn_sig) = if WRITE {
         (
             BINWRITE_TRAIT,
@@ -76,6 +72,24 @@ pub(crate) fn generate_impl<const WRITE: bool>(
         )
     };
 
+    let fn_impl = match binrw_input {
+        ParseResult::Ok(binrw_input) => {
+            if WRITE {
+                write_options::generate(binrw_input, derive_input)
+            } else {
+                read_options::generate(binrw_input, derive_input)
+            }
+        }
+        // If there is a parsing error, an impl for the trait still needs to be
+        // generated to avoid misleading errors at all call sites that use the
+        // trait, so emit the trait and just stick the errors inside the generated
+        // function
+        ParseResult::Partial(_, error) | ParseResult::Err(error) => error.to_compile_error(),
+    };
+
+    let name = &derive_input.ident;
+    let (impl_generics, ty_generics, where_clause) = derive_input.generics.split_for_impl();
+
     quote! {
         #[allow(non_snake_case)]
         impl #impl_generics #trait_name for #name #ty_generics #where_clause {
@@ -85,10 +99,6 @@ pub(crate) fn generate_impl<const WRITE: bool>(
                 #fn_impl
             }
         }
-
-        #meta_impls
-
-        #arg_type_declaration
     }
 }
 
